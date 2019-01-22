0174bf49a2b144cf3879fd5e10328c4462d54133
Kuni <kuni.ujiie@grapecity.com>
2019-01-22 15:56:39 +0900

    Add Wijmo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { WjGridModule } from 'wijmo/wijmo.angular2.grid';
+import 'wijmo/cultures/wijmo.culture.ja.js';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    WjGridModule,
     BrowserModule
   ],
   providers: [],
